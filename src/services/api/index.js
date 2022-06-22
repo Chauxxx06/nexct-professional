87b3d7dfda0018f843064ec34beacfadf97ca455
Chauxxx06 <freemanchx@hotmail.com>
2022-06-22 23:48:54 +0000

    creando consulta a productos

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -11,7 +11,7 @@ const endPoints = {
     postProducts: `${API}/api/${VERSION}/products/`,
     putProducts: (id) => `${API}/api/${VERSION}/products/${id}`,
     deleteProducts: (id) => `${API}/api/${VERSION}/products/${id}`,
-    getProducts: `${API}/api/${VERSION}/products/`,
+    getProducts: (limit, offset) => `${API}/api/${VERSION}/products?limit=${limit}&offset=${offset}`,
   },
   users: {
     getUsers: `${API}/api/${VERSION}/users`,
